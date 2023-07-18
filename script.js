79aba2950ec94e092e055a123a1375336fb19e7d
Kevansh Gopani <130953386+KevanshGopani@users.noreply.github.com>
2023-07-18 09:19:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 //your code here
 
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal', 'yamuna the aji'];
+let newArr =[];
+
+for( const value of touristSpots){
+  newArr.push(value.replace(/The |An | a | an | the/gi, ""))
+}
+
+newArr.sort()
+
+for(let value of newArr){
+console.log(value)
+}
