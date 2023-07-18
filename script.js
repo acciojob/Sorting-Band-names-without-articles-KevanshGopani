027427cdfabaf4b2f62f4da8d3fee67e53832c8d
Kevansh Gopani <130953386+KevanshGopani@users.noreply.github.com>
2023-07-18 09:51:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-//your code here
+let list = document.getElementById("band");
 
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal', 'yamuna the aji'];
+
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 let newArr =[];
 
 for( const value of touristSpots){
   newArr.push(value.replace(/The |An | a | an | the/gi, ""))
 }
-
 newArr.sort()
+console.log(newArr)
 
-for(let value of newArr){
-console.log(value)
-}
+for (let lists in newArr) {
+         var li = document.createElement('li');
+         li.innerText = newArr[lists];
+         list.appendChild(li);
+      }
